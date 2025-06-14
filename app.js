496f97b32bee4f7ba310b7b632f38ce5dbb9e4b6
Ajay Kumar dhurwe <54279953+ajaykumardhurwe@users.noreply.github.com>
2025-06-14 15:04:47 +0530

    adding frontend url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors(
   {
-    origin: ["https://backend-blog-kappa-nine.vercel.app/"],
+    origin: ["https://ajaykumardhurwe.github.io/frontend.blog/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
